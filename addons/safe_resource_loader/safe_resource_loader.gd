29e27fb432ef6e8db3c81b1c0d29ed53cd75f70c
Jan Thomä <janthomae@janthomae.de>
2025-04-17 08:47:34 +0200

    fix: strip comments to prevent them messing with the detection

--- addons/safe_resource_loader/safe_resource_loader.gd
+++ addons/safe_resource_loader/safe_resource_loader.gd
@@ -31,6 +31,9 @@ static func load(path:String, type_hint:String = "", \
 	var file = FileAccess.open(path, FileAccess.READ)
 	var file_as_text = file.get_as_text()
 	file.close()
+	
+	# strip out any comments to make detection easier
+	file_as_text = _strip_comments(file_as_text)
 
 	# Use a regex to find any instance of an embedded GDScript resource.
 	var regex:RegEx = RegEx.new()
@@ -72,4 +75,72 @@ static func load(path:String, type_hint:String = "", \
 
 	# otherwise use the normal resource loader to load it.
 	return ResourceLoader.load(path, type_hint, cache_mode)
+	
+## The resource format supports single-line comments, starting with ";"
+## and ending with a newline. We strip them out here to avoid having to factor
+## them into our detection logic. This is approaching a parser, albeit 
+## a very simple one.
+static func _strip_comments(text:String) -> String:
+
+	# We have comments and we have strings. We don't want to find 
+	# comment-like things inside strings, but at the same time we cannot
+	# just ignore everything between quotes, because these quotes might
+	# be inside a comment. So we will need an actual parser for this.
+	# We go over the text character by character, and keep track of whether
+	# we are inside a comment or a string. If we are inside a comment, we
+	# ignore everything until the end of the line. If we are inside a string,
+	# we don't look for comments.
+	
+	# The result string. We append to it when we have a known good set of 
+	# characters.
+	var result:String = ""
+	# The index of last character we added to the result string.
+	var write_offset:int = 0
+	
+	var in_comment:bool = false
+	var in_string:bool = false
+	
+	for i in text.length():
+		var char = text.unicode_at(i)
+		# if we are in a string, we ignore everything until the end of the string.
+		if in_string:
+			if char == 34:  # 34 is unicode for double quote
+				# check if the preceding character is a backslash, if so, we are not done with the string yet
+				if i > 0 and text.unicode_at(i - 1) == 92:  # 92 is unicode for backslash
+					continue
+				in_string = false
+			continue
+		
+		# if we are in a comment, we ignore everything until the end of the line.
+		if in_comment:
+			if char == 10:  # 10 is unicode for newline
+				in_comment = false
+				result += "\n"
+				write_offset = i + 1
+			continue
+		
+		if char == 59:  # 59 is unicode for semicolon
+			# we are now starting a comment
+			in_comment = true
+			if i > write_offset:
+				# we have seen some text before the comment, so we can write it to the result string
+				result += text.substr(write_offset, i - write_offset)
+				write_offset = i	
+	
+		if char == 34:  # 34 is unicode for double quote
+			# we are now starting a string
+			in_string = true
+			# because we don't want to strip strings, we don't write anything just yet
+	
+	# if we are still in comment right now, we can ignore the rest
+	if in_comment:
+		return result
+	
+	# technically we can still be in string right now and that would make the whole thing
+	# an invalid resource file, but that is something we hand over to the real resource loader
+	# so we just write the rest of the string to the result string
+	if write_offset < text.length():
+		result += text.substr(write_offset, text.length() - write_offset)
+		
+	return result
 	
